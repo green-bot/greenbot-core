836933d3aaef05bb817be585d341dcf24e3866fa
Thomas Howe <therealthomashowe@gmail.com>
2015-02-06 14:16:19 -0500

    Updated contact bug in information script

--- scripts/information.rb
+++ scripts/information.rb
@@ -10,47 +10,41 @@ HOUR = 60 * 60
 timeout = ENV['CONVERSATION_TIMEOUT'].to_i || 4 * HOUR
 
 begin
+  puts ENV['PROMPT_1']
+  puts ENV['PROMPT_2']
   Timeout::timeout(timeout) {
-    puts ENV['PROMPT_1']
-    puts ENV['PROMPT_2']
+    begin
+      tasks = %w(hours specials address contact quit)
+      my_task = select("How can I help?", tasks)
 
-    puts("How can I help? (Hours, Specials, Address, Contact, Quit)")
-    choice = gets
-    choice.downcase!
-    choice.remember("choice")
-
-    case choice.each_char.first
-    when "h"
-      puts ENV['HOURS']
-    when "s"
-      puts ENV['SPECIALS']
-    when "a"
-      puts ENV['ADDRESS']
-    when "c"
-        if agree("Would you like someone to contact you?", false)
-          contact_me = true
-          contact_me.remember("remember_me")
-          name = ask("When we call, who should we ask for?")
-          name.remember("who_to_ask_for")
-          if agree("Is there another number we should try?", false)
-            better_number = ask("Please enter that number with an area code", 
-                                 lambda { |p| p.delete("^0-9").
-                                                sub(/\A(\d{3})/, '(\1) ').
-                                                sub(/(\d{4})\Z/, '-\1') } ) do |q|
-                              q.validate = lambda { |p| p.delete("^0-9").length == 10 }
-                              q.responses[:not_valid] = "Enter a phone numer with area code."
-                            end
-             better_number.remember("better_number")
+      case my_task
+      when "hours"
+        puts ENV['HOURS']
+      when "specials"
+        puts ENV['SPECIALS']
+      when "address"
+        puts ENV['ADDRESS']
+      when "contact"
+          if confirm("Would you like someone to contact you?")
+            contact_me = true
+            contact_me.remember("remember_me")
+            puts("When we call, who should we ask for?")
+            name = gets.chomp
+            name.remember("who_to_ask_for")
+            if confirm("Is there another number we should try?")
+              puts("Please enter that number with an area code")
+              better_number = gets.chomp
+              better_number.remember("better_number")
+            end
+          else
+            puts("No problem at all.")
           end
-        else
-          puts("No problem at all.")
-        end
-    end
-    puts ENV['SIGNATURE']
+      when "quit"
+        break
+      end
+    end while true
   }
 rescue Timeout::Error  => e
   puts "If you want to restart this conversation, text us again!"
-  puts ENV['SIGNATURE']
 end
-
- 
+puts ENV['SIGNATURE']

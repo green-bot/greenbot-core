71d100ee37759e429f6967f677b9e2c53525f092
Thomas Howe <thomashowe@Thomass-MacBook-Pro.local>
2015-06-13 17:13:27 -0400

    Added survey improvements

--- scripts/survey.rb
+++ scripts/survey.rb
@@ -16,7 +16,7 @@ begin
     prompts = %w( DATA_PROMPT_1 DATA_PROMPT_2 DATA_PROMPT_3 DATA_PROMPT_4 DATA_PROMPT_5 DATA_PROMPT_6 DATA_PROMPT_7 DATA_PROMPT_8 DATA_PROMPT_9 DATA_PROMPT_10 )
     data_prompts = []
     prompts.each do |p|
-      unless ENV[p].nil?
+      if ENV[p].size > 0
         answer = ask(ENV[p])
         answer.remember(p)
       end

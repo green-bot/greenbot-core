dac8418f32d064330322ee2306d92ac2a507058f
Thomas Howe <thomashowe@Thomass-MacBook-Pro.local>
2015-06-13 17:08:07 -0400

    Added survey improvements

--- scripts/survey.rb
+++ scripts/survey.rb
@@ -13,23 +13,14 @@ begin
     tell ENV['PROMPT_2']
 
     name = confirmed_gets(ENV['NAME_PROMPT']) if ENV['NAME_PROMPT']
-
-    data_prompts = {}
-    data_prompts['DATA_PROMPT_1'] = ENV['DATA_PROMPT_1'] unless ENV['DATA_PROMPT_1'].nil?
-    data_prompts['DATA_PROMPT_2'] = ENV['DATA_PROMPT_2'] unless ENV['DATA_PROMPT_2'].nil?
-    data_prompts['DATA_PROMPT_3'] = ENV['DATA_PROMPT_3'] unless ENV['DATA_PROMPT_3'].nil?
-    data_prompts['DATA_PROMPT_4'] = ENV['DATA_PROMPT_4'] unless ENV['DATA_PROMPT_4'].nil?
-    data_prompts['DATA_PROMPT_5'] = ENV['DATA_PROMPT_5'] unless ENV['DATA_PROMPT_5'].nil?
-    data_prompts['DATA_PROMPT_6'] = ENV['DATA_PROMPT_6'] unless ENV['DATA_PROMPT_6'].nil?
-    data_prompts['DATA_PROMPT_7'] = ENV['DATA_PROMPT_7'] unless ENV['DATA_PROMPT_7'].nil?
-    data_prompts['DATA_PROMPT_8'] = ENV['DATA_PROMPT_8'] unless ENV['DATA_PROMPT_8'].nil?
-    data_prompts['DATA_PROMPT_9'] = ENV['DATA_PROMPT_9'] unless ENV['DATA_PROMPT_9'].nil?
-    data_prompts['DATA_PROMPT_10'] = ENV['DATA_PROMPT_10'] unless ENV['DATA_PROMPT_10'].nil?
-
-    data_prompts.each do |k,v|
-      answer = ask(v)
-      answer.remember(k)
-    end
+    prompts = %w( DATA_PROMPT_1 DATA_PROMPT_2 DATA_PROMPT_3 DATA_PROMPT_4 DATA_PROMPT_5 DATA_PROMPT_6 DATA_PROMPT_7 DATA_PROMPT_8 DATA_PROMPT_9 DATA_PROMPT_10 )
+    data_prompts = []
+    prompts.each do |p|
+      unless ENV[p].nil?
+        answer = ask(p)
+        answer.remember(p)
+      end
+    end    
     tell ENV['SIGNATURE']
   }
 rescue Timeout::Error  => e

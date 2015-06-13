472e60bb19edb6bf6987212db56b26ffbb73501c
Thomas Howe <thomashowe@Thomass-MacBook-Pro.local>
2015-06-13 19:28:29 -0400

    Fixed leads bug

--- scripts/leads.rb
+++ scripts/leads.rb
@@ -8,7 +8,7 @@ HOUR = 60 * 60
 timeout = ENV['CONVERSATION_TIMEOUT'].to_i || 4 * HOUR
 CONFIRMATION_PROMPT = ENV['CONFIRMATION_PROMPT'] || "Would you like someone to contact you?"
 NAME_PROMPT = ENV['NAME_PROMPT'] || "When we call, who should we ask for?"
-OTHER_CONTACT_CONFIRM = ENV['OTHER_CONTACT'] || "Is there another number we should try?"
+OTHER_CONTACT_CONFIRM = ENV['OTHER_CONTACT_CONFIRM'] || "Is there another number we should try?"
 OTHER_CONTACT_PROMPT = ENV['OTHER_CONTACT_PROMPT'] || "Please enter that number with an area code"
 NOTE_PROMPT = ENV['NOTE_PROMPT'] || "How can we help you? Please use as many messages as you need."
 begin

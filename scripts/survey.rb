98e209c04bf83046e5b581e3cb92f04fd3680933
Thomas Howe <thomashowe@Thomass-MacBook-Pro.local>
2015-06-13 17:03:44 -0400

    Added survey improvements

--- scripts/survey.rb
+++ scripts/survey.rb
@@ -15,16 +15,16 @@ begin
     name = confirmed_gets(ENV['NAME_PROMPT']) if ENV['NAME_PROMPT']
 
     data_prompts = {}
-    data_prompts['DATA_PROMPT_1'] = ENV['DATA_PROMPT_1'] if ENV['DATA_PROMPT_1']
-    data_prompts['DATA_PROMPT_2'] = ENV['DATA_PROMPT_2'] if ENV['DATA_PROMPT_2']
-    data_prompts['DATA_PROMPT_3'] = ENV['DATA_PROMPT_3'] if ENV['DATA_PROMPT_3']
-    data_prompts['DATA_PROMPT_4'] = ENV['DATA_PROMPT_4'] if ENV['DATA_PROMPT_4']
-    data_prompts['DATA_PROMPT_5'] = ENV['DATA_PROMPT_5'] if ENV['DATA_PROMPT_5']
-    data_prompts['DATA_PROMPT_6'] = ENV['DATA_PROMPT_6'] if ENV['DATA_PROMPT_6']
-    data_prompts['DATA_PROMPT_7'] = ENV['DATA_PROMPT_7'] if ENV['DATA_PROMPT_7']
-    data_prompts['DATA_PROMPT_8'] = ENV['DATA_PROMPT_8'] if ENV['DATA_PROMPT_8']
-    data_prompts['DATA_PROMPT_9'] = ENV['DATA_PROMPT_9'] if ENV['DATA_PROMPT_9']
-    data_prompts['DATA_PROMPT_10'] = ENV['DATA_PROMPT_10'] if ENV['DATA_PROMPT_10']
+    data_prompts['DATA_PROMPT_1'] = ENV['DATA_PROMPT_1'] unless ENV['DATA_PROMPT_1'].nil?
+    data_prompts['DATA_PROMPT_2'] = ENV['DATA_PROMPT_2'] unless ENV['DATA_PROMPT_2'].nil?
+    data_prompts['DATA_PROMPT_3'] = ENV['DATA_PROMPT_3'] unless ENV['DATA_PROMPT_3'].nil?
+    data_prompts['DATA_PROMPT_4'] = ENV['DATA_PROMPT_4'] unless ENV['DATA_PROMPT_4'].nil?
+    data_prompts['DATA_PROMPT_5'] = ENV['DATA_PROMPT_5'] unless ENV['DATA_PROMPT_5'].nil?
+    data_prompts['DATA_PROMPT_6'] = ENV['DATA_PROMPT_6'] unless ENV['DATA_PROMPT_6'].nil?
+    data_prompts['DATA_PROMPT_7'] = ENV['DATA_PROMPT_7'] unless ENV['DATA_PROMPT_7'].nil?
+    data_prompts['DATA_PROMPT_8'] = ENV['DATA_PROMPT_8'] unless ENV['DATA_PROMPT_8'].nil?
+    data_prompts['DATA_PROMPT_9'] = ENV['DATA_PROMPT_9'] unless ENV['DATA_PROMPT_9'].nil?
+    data_prompts['DATA_PROMPT_10'] = ENV['DATA_PROMPT_10'] unless ENV['DATA_PROMPT_10'].nil?
 
     data_prompts.each do |k,v|
       answer = ask(v)

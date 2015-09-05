9b482d7023d133033f387ee49f5aeaf918a3712f
Thomas Howe <ghostofbasho@gmail.com>
2015-09-05 13:55:46 -0400

    Needed default params on leads

--- scripts/leads.rb
+++ scripts/leads.rb
@@ -3,11 +3,11 @@
 #
 require './lib/greenbot.rb'
 
-CONFIRMATION_PROMPT = ENV['CONFIRMATION_PROMPT']
-NAME_PROMPT = ENV['NAME_PROMPT']
-OTHER_CONTACT_CONFIRM = ENV['OTHER_CONTACT_CONFIRM']
-OTHER_CONTACT_PROMPT = ENV['OTHER_CONTACT_PROMPT']
-NOTE_PROMPT = ENV['NOTE_PROMPT']
+CONFIRMATION_PROMPT = ENV['CONFIRMATION_PROMPT'] || 'Would you like someone to contact you?'
+NAME_PROMPT = ENV['NAME_PROMPT'] || 'Who should we ask for?'
+OTHER_CONTACT_CONFIRM = ENV['OTHER_CONTACT_CONFIRM'] || 'Is there another way you would like to be contacted?'
+OTHER_CONTACT_PROMPT = ENV['OTHER_CONTACT_PROMPT'] || 'How else would you like to be contacted?'
+NOTE_PROMPT = ENV['NOTE_PROMPT'] || 'Please tell us how we can help. Send a single Q to quit'
 
 tell ENV['PROMPT_1']
 tell ENV['PROMPT_2']

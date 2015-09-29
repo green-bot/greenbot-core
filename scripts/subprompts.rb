c5e051617555eb43836c23c02ee78175c92d7c89
Thomas Howe <ghostofbasho@gmail.com>
2015-09-29 11:34:41 -0400

    Added downcase for menu selection, re-indent when

--- scripts/subprompts.rb
+++ scripts/subprompts.rb
@@ -6,31 +6,31 @@ require './lib/greenbot.rb'
 tell ENV['PROMPT_1']
 tell ENV['PROMPT_2']
 begin
-  tasks = ENV['MENU_CHOICES'].split
+  tasks = ENV['MENU_CHOICES'].split(',').map(&:strip)
   tasks << 'quit'
-  tasks << 'contact'
-  my_task = select('How can I help?', tasks)
+  tasks << 'contactme'
+  my_task = select(ENV['MENU_PROMPT'], tasks).downcase
 
   case my_task
-  when 'contact'
-      if confirm(ENV['CONFIRM_CONTACT'])
-        contact_me = true
-        contact_me.remember('contact_me')
-        name = ask(ENV['WHO_TO_ASK_FOR'])
-        name.remember('who_to_ask_for')
-        if confirm(ENV['PREFER_ALTERNATE_CONTACT'])
-          better_number = ask(ENV['ALTERNATE_CONTACT_COLLECTION'])
-          better_number.remember('better_number')
-        end
-      else
-        tell(ENV['DONT_CONTACT_PROMPT'])
-        contact_me = false
-        contact_me.remember('contact_me')
+  when 'contactme'
+    if confirm(ENV['CONFIRM_CONTACT'])
+      contact_me = true
+      contact_me.remember('contact_me')
+      name = ask(ENV['WHO_TO_ASK_FOR'])
+      name.remember('who_to_ask_for')
+      if confirm(ENV['PREFER_ALTERNATE_CONTACT'])
+        better_number = ask(ENV['ALTERNATE_CONTACT_COLLECTION'])
+        better_number.remember('better_number')
       end
+    else
+      tell(ENV['DONT_CONTACT_PROMPT'])
+      contact_me = false
+      contact_me.remember('contact_me')
+    end
   when 'quit'
     break
   else
-    case tasks.index(my_task)
+    case tasks.map(&:downcase).index(my_task)
     when 0
       tell ENV['FIRST_CHOICE']
     when 1

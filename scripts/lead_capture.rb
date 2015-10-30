5b2e5478702bbbc677b9f0480dba2da59f03dbfe
Thomas Howe <ghostofbasho@gmail.com>
2015-10-30 17:16:15 -0400

    New lead capture bot

--- scripts/lead_capture.rb
+++ scripts/lead_capture.rb
@@ -3,25 +3,36 @@
 #
 require './lib/greenbot.rb'
 
-tell ENV['PROMPT_1']
-tell ENV['PROMPT_2'] if ENV['PROMPT_2']
-
-name = ask(ENV['NAME_PROMPT'])
-name.remember('name')
-zip_prompt = ask(ENV['ZIP_PROMPT'])
-zip_prompt.remember('zip_prompt')
-email_prompt = ask(ENV['EMAIL_PROMPT'])
-email_prompt.remember('email_prompt')
-
-tell ENV['THANK_YOU_PROMPT']
-
-additional_answer = false
-if confirm(ENV['ADDITIONAL_PROMPT_Q'])
-  additional_answer = true
-  tell ENV['ADDITIONAL_PROMPT_Y']
-else
-  tell ENV['ADDITIONAL_PROMPT_N']
+# Extend string to include the opposite of empty
+def ptell(prompt)
+  return if prompt.empty?
+  tell prompt
 end
-additional_answer.remember('additional_answer')
 
-tell ENV['SIGNATURE']
+def pask(prompt, name)
+  return if prompt.empty?
+  answer = ask(prompt)
+  answer.remember(name)
+end
+
+tell ENV['PROMPT_1']
+ptell ENV['PROMPT_2']
+
+pask ENV['NAME_PROMPT'], 'name'
+pask ENV['ZIP_PROMPT'], 'zip'
+pask ENV['EMAIL_PROMPT'], 'email'
+
+ptell ENV['THANK_YOU_PROMPT']
+
+unless ENV['ADDITIONAL_PROMPT_Q'].empty?
+  additional_answer = false
+  if confirm(ENV['ADDITIONAL_PROMPT_Q'])
+    additional_answer = true
+    tell ENV['ADDITIONAL_PROMPT_Y']
+  else
+    tell ENV['ADDITIONAL_PROMPT_N']
+  end
+  additional_answer.remember('additional_answer')
+end
+
+ptell ENV['SIGNATURE']

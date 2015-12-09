c32dfe5030fa150cecb89050f0a1ee36dc1af5f6
Thomas Howe <ghostofbasho@gmail.com>
2015-12-09 14:05:40 -0500

    Cleaned up

--- kisst/cloud/require-user.js
+++
@@ -1,9 +0,0 @@
-// Use this middleware to require that a user is logged in
-module.exports = function(req, res, next) {
-  console.log("Checking for logged in user");
-  if (Parse.User.current()) {
-    next();
-  } else {
-    res.send("You need to be logged in to see this page.");
-  }
-}
--- kisst/cloud/routes/media.js
+++
@@ -1,9 +0,0 @@
-var bc = require('cloud/bootcards-functions');
-
-exports.list = function(req, res){
-
-	res.renderPjax('data', {
-  		menu: bc.getActiveMenu(menu, 'data')
-	});
-
-};
